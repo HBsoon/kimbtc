933b7742e5f15e82827d9a971cf0e8f422517903
kim <khj3093@gmail.com>
2023-10-19 22:05:11 +0900

    ok

--- build_was-tomcat/init-database.sh
+++ build_was-tomcat/init-database.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Initializing MySQL database..."
-mysql -h <MYSQL IP 기입> -u root -p<PW 기입> <<EOF
+mysql -h 10.155.16.5 -u master -pqwe123!@# <<EOF
 CREATE DATABASE IF NOT EXISTS petclinic;
 EOF
 
 echo "Adding sample data to the database..."
-mysql -h <MYSQL IP 기입> -u root -p<PW 기입> petclinic < /docker-entrypoint-initdb.d/data.sql
+mysql -h 10.155.16.5 -u master -pqwe123!@# petclinic < /docker-entrypoint-initdb.d/data.sql
 
 echo "Database initialization and data insertion complete."
